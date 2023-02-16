0f0b68ca2a93023c325e617f2cf3d0c86add3760
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-16 23:15:08 +0200

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from 'components/Searchbar/Searchbar.jsx';
+export * from 'components/Loader/Loader.jsx';
+export * from 'components/ImageGallery/ImageGallery.jsx';
+export * from 'components/Button/Button.jsx';
+export * from 'components/Modal/Modal.jsx';
