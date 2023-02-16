9012f5de6ea497f911832665bd1eb24b3c858608
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-16 23:15:10 +0200

    Create Loader.jsx

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,20 @@
+import { Loader } from './Loader.styled';
+
+export function Loading() {
+  return (
+    <Loader>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Loader>
+  );
+}
